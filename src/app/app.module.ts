8bcfcdabb0212f76a0f246c852c4d878d463bb90
410-2001 <aaaa>
2018-01-06 16:11:50 +0900

    5~6.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import {FormsModule} from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import {HeroService} from './hero.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HEROES} from '../nock-heroes';
+import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  /*heroes = HEROES;*/
+  heroes: Hero[];
+  selectedHero: Hero;
 
-  ngOnInit() {
+  // hero = 'Windstorm';
+
+  // 14~17이 19~24와 결과 같음
+   /*hero: Hero = {
+     id: 1,
+     name: 'Windstorm'
+   };
+*/
+  /* hero: Hero;*/
+
+  constructor(public heroService: HeroService) {
+    // name뒤에?를 썼기 때문에 name값 안써도 에러 안남
+    // this.hero = new Hero(1);
+    // this.hero.id = 1;
+    // this.hero.name = 'Windstorm';
   }
 
+  ngOnInit() {
+    //비동기식Promise
+    /*this.heroService.getHeroes()
+      .then(data => this.heroes = data);*/
+    this.heroService.getHeroes()
+      .subscribe(data => this.heroes = data);
+  }
+
+  onSelect(hero: Hero) {
+    console.log(hero);
+    this.selectedHero = hero;
+  }
 }
+
